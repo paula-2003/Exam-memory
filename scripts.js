43879d6d99484b028fda4a617f5860131c6c6a88
paula-2003 <Paula.Gerhardt@stud.hs-coburg.de>
2023-02-12 16:32:35 +0100

    add game logic

--- scripts.js
+++ scripts.js
@@ -15,10 +15,41 @@ function revealCard() {   // Wird aufgerufen wenn auf Karte geklickt
         firstSelectedCard = this;   // Angeklickte Karte(this) wird als erste Karte festgelegt
     } else {
         secondSelectedCard = this; // angeklickte Karte wird als zweite Karte festgelegt
+        matchCards(); // funktion wird augerufen
+    }
+}
+function matchCards() {  //Vergleicht Datensätze ob sie gleich sind
+    const isMatch = firstSelectedCard.dataset.framework === secondSelectedCard.dataset.framework; // lokale Variable zum vergleichen  const weil sie sich nicht ändert
 
+    isMatch ? foundMatch() : noMatch(); // kurze Schreibweise für if isMatch true dann foundMatch ansonsten noMatch
+}
+
+function foundMatch() {
+    firstSelectedCard.removeEventListener('click', revealCard); // enfernt den eventListener, damit nichts passiert wenn man auf das paar klickt
+    secondSelectedCard.removeEventListener('click', revealCard); // enfernt den eventListener, damit nichts passiert wenn man auf das paar klickt
+    Highscore(); // ruft Funktion auf um Highscore zu ändern
+    resetVariables(); //  ruft die Funktion auf
+    remainingCards--; //verringert die Anzahl der verbleibenden Karten 
+    if (remainingCards === 0) {  //überprüft, ob alle Karten gematcht wurden
+        allCardsMatched = true; // setzt allCardsMatched auf True
+        showEndMessage(); // ruft Funktion auf und blendet Satz ein
     }
 }
 
+function noMatch() {
+    isBoardLocked = true; // Spielfeld wird gesperrt
+
+    setTimeout(() => {
+        firstSelectedCard.classList.remove('flip'); // Dreht die Karte wieder um
+        secondSelectedCard.classList.remove('flip'); // Dreht die zweite Karten wieder um
+        Highscore(); // ruft die Funktion zum Highfscore auf
+        resetVariables(); // ruft die Funktion auf
+    }, 3000); // nach 3 Sekunden drehen sie sich um
+}
+function resetVariables() {
+    [isCardFlipped, isBoardLocked] = [false, false]; // setzt die Variablen auf ihre Anfangswerte zurück
+    [firstSelectedCard, secondSelectedCard] = [null, null]; // setzt die Variablen auf ihre Anfangswerte zurück
+}
 function shuffleCards() { // vermischt die Karten
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 16); // weist eine zufällige Ordnungszahl zu
